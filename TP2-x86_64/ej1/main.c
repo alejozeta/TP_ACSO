54c25a0ac2268d6744da0340e1e1e0f06aca96b3
alejozeta <128553535+kekontas@users.noreply.github.com>
2025-04-19 10:43:47 -0300

    a

--- TP2-x86_64/ej1/main.c
+++ TP2-x86_64/ej1/main.c
@@ -20,7 +20,7 @@ void test_create_destroy_list(){
 void test_create_destroy_node(){
 	printf("Creando nodo vacio\n");
 	string_proc_node* node	= string_proc_node_create_asm(0, "hash");
-	printf((strcmp(node->hash, "hash") == 0 && node->type == 0) ? "Nodo creado correctamente\n" : "Error al crear el nodo\n");
+	printf((strcmp(node->hash, "hash") == 1 && node->type == 0) ? "Nodo creado correctamente\n" : "Error al crear el nodo\n");
 	printf("Destruyendo nodo\n");
 	string_proc_node_destroy(node);
 }
