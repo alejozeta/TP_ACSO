e16ada8e30505949db1efe21585d544bd790d839
alejozeta <128553535+kekontas@users.noreply.github.com>
2025-05-08 11:35:28 -0300

    a

--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -46,51 +46,58 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
 /**
  * TODO
  */
-int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp,
-    int blockNum) {  
-    // Check if the inode is valid and blockNum is non-negative
+int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
     if (inp == NULL || blockNum < 0) {
-        return -1; // Invalid parameters
+        return -1;
     }
 
-    // Check if the blockNum is within the direct blocks
-    if (blockNum < 10) {
-        return inp->i_addr[blockNum]; // Return the direct block address
+    int filesize = inode_getsize(inp);
+    int maxBlocks = (filesize + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
+    if (blockNum >= maxBlocks) {
+        return -1;
     }
 
-    // Calculate the indirect block index
-    blockNum -= 10;
-
-    // Check if the blockNum is within the range of single indirect blocks
-    if (blockNum < 128) {
-        char buf[BLOCK_SIZE];
-        if (diskimg_readsector(fs->dfd, inp->i_addr[10], buf) != BLOCK_SIZE) {
-            return -1; // Error reading the indirect block
-        }
-        return ((int *)buf)[blockNum]; // Return the block address
+    // Caso 1: archivo chico (sin ILARG): usar i_addr[blockNum]
+    if ((inp->i_mode & ILARG) == 0) {
+        if (blockNum >= 8) return -1;
+        return inp->i_addr[blockNum];
     }
 
-    // Calculate the double indirect block index
-    blockNum -= 128;
+    // Caso 2: archivo grande (ILARG activado)
+    if (blockNum < 7 * 256) {
+        int which = blockNum / 256;
+        int offset = blockNum % 256;
 
-    if (blockNum < 128 * 128) {
-        char buf1[BLOCK_SIZE], buf2[BLOCK_SIZE];
-        int indirectBlockIndex = blockNum / 128;
-        int indirectBlockOffset = blockNum % 128;
+        uint16_t indir = inp->i_addr[which];
+        if (!indir) return -1;
 
-        if (diskimg_readsector(fs->dfd, inp->i_addr[11], buf1) != BLOCK_SIZE) {
-            return -1; // Error reading the double indirect block
-        }
+        uint16_t table[256];
+        if (diskimg_readsector(fs->dfd, indir, table) != DISKIMG_SECTOR_SIZE)
+            return -1;
 
-        int indirectBlockAddr = ((int *)buf1)[indirectBlockIndex];
-        if (diskimg_readsector(fs->dfd, indirectBlockAddr, buf2) != BLOCK_SIZE) {
-            return -1; // Error reading the indirect block
-        }
-
-        return ((int *)buf2)[indirectBlockOffset]; // Return the block address
+        return table[offset];
     }
 
-    return -1; // Block number out of range
+    // Caso 3: doble indirecto
+    blockNum -= 7 * 256;
+
+    uint16_t dbl = inp->i_addr[7];
+    if (!dbl) return -1;
+
+    uint16_t lvl1[256];
+    if (diskimg_readsector(fs->dfd, dbl, lvl1) != DISKIMG_SECTOR_SIZE)
+        return -1;
+
+    int i1 = blockNum / 256;
+    int i2 = blockNum % 256;
+
+    if (!lvl1[i1]) return -1;
+
+    uint16_t lvl2[256];
+    if (diskimg_readsector(fs->dfd, lvl1[i1], lvl2) != DISKIMG_SECTOR_SIZE)
+        return -1;
+
+    return lvl2[i2];
 }
 
 int inode_getsize(struct inode *inp) {
