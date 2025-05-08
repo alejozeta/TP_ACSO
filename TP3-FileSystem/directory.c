7824f2a51957ffa3276fd7aa37b954b9abff18ef
alejozeta <128553535+kekontas@users.noreply.github.com>
2025-05-08 14:56:10 -0300

    a

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -10,7 +10,37 @@
  * TODO
  */
 int directory_findname(struct unixfilesystem *fs, const char *name,
-		int dirinumber, struct direntv6 *dirEnt) {
-  //Implement your code here
-  return 0;
+  int dirinumber, struct direntv6 *dirEnt) {
+    if (!fs || !name || !dirEnt) {
+        return -1;
+        }
+
+    struct inode dir_inode;
+    if (inode_iget(fs, dirinumber, &dir_inode) < 0) {
+        return -1;
+        }
+
+    int filesize = inode_getsize(&dir_inode);
+    int num_dirents = filesize / sizeof(struct direntv6);
+
+    for (int i = 0; i < num_dirents; i++) {
+        struct direntv6 d;
+        int block = i * sizeof(struct direntv6) / DISKIMG_SECTOR_SIZE;
+        int offset = (i * sizeof(struct direntv6)) % DISKIMG_SECTOR_SIZE;
+
+        char buf[DISKIMG_SECTOR_SIZE];
+        if (file_getblock(fs, dirinumber, block, buf) < 0) {
+            return -1;
+            }
+
+        memcpy(&d, buf + offset, sizeof(struct direntv6));
+
+        if (strncmp(d.d_name, name, 14) == 0) {
+            *dirEnt = d;
+            return 0;}
+    }
+
+    return -1;  // nombre no encontrado
 }
+
+
