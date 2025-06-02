69644bdf9339a777317672f43f8bed96ada9c959
alejozeta <128553535+kekontas@users.noreply.github.com>
2025-06-01 22:13:17 -0300

    a

--- TP4-Shell/src/ej2/shell.c
+++ TP4-Shell/src/ej2/shell.c
@@ -105,8 +105,9 @@ int main() {
 
             pid_t pid = fork();
             if (pid == -1) {
-                perror("fork");
-                exit(1);
+                fprintf(stderr, "Error: no se pudo crear proceso\n");
+                // Para no romper la tubería completa, continuar el loop:
+                continue;
             } else if (pid == 0) {
                 if (prev_fd != -1) {
                     dup2(prev_fd, STDIN_FILENO);
