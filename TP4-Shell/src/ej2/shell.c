b3e4f9eef7bba049defde290f859ac06ffd71ea4
alejozeta <128553535+kekontas@users.noreply.github.com>
2025-06-01 22:07:57 -0300

    a

--- TP4-Shell/src/ej2/shell.c
+++ TP4-Shell/src/ej2/shell.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_COMMANDS 256
+#define MAX_COMMANDS 200
 #define MAX_ARGS 64
 
 // Parsea un string en args[], sin malloc, respetando comillas
