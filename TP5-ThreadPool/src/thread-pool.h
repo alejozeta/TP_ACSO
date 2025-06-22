76611645d29fa2e97c9d0d7e7161fec7279ff1f4
alejozeta <128553535+kekontas@users.noreply.github.com>
2025-06-22 19:20:58 -0300

    a

--- TP5-ThreadPool/src/thread-pool.h
+++ TP5-ThreadPool/src/thread-pool.h
@@ -94,10 +94,10 @@ class ThreadPool {
     * assignment operator.  By doing so, the compiler will ensure we never clone
     * a ThreadPool. */
     queue<function<void(void)>> taskQueue;
-    condition_variable_any queueCV;
+    condition_variable queueCV;
 
     mutex waitLock;
-    condition_variable_any waitCV;
+    condition_variable waitCV;
     atomic<int> pendingTasks{0}; // Tiene que ser atómica para que el dispatcher pueda decrementarla sin problemas de carrera
 
 
